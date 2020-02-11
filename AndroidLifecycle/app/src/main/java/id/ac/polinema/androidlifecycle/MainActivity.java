aa024605b6400a34d14250d7c06e67e725b8517d
sindyRosa <sindyros123@gmail.com>
2020-02-11 09:42:48 +0700

    Activity dan Desain Layout

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package id.ac.polinema.androidlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +12,32 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
 	// TODO: tambahkan callback onStart() di sini
-
-
+	protected void onStart(){
+		super.onStart();
+		Toast.makeText(this, "App on Start", Toast.LENGTH_SHORT).show();
+	}
 	// TODO: tambahkan callback onStop() di sini
-
+	protected void onStop(){
+		super.onStop();
+		Toast.makeText(this, "App on Stop ", Toast.LENGTH_SHORT).show();
+	}
 	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Toast.makeText(this, "Application On Resume", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Toast.makeText(this, "Application On Pause", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Toast.makeText(this, "Application On Destroy", Toast.LENGTH_SHORT).show();
+	}
 }
